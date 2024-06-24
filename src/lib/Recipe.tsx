0d0abc47f85691f89a789cd9097ecd70d2f636f1
ZacBouh <50382656+ZacBouh@users.noreply.github.com>
2024-06-24 11:44:52 +0200

    Recipe class

---
+++ src/lib/Recipe.tsx
@@ -0,0 +1,54 @@
+export class Recipe {
+  id?: string;
+  name: string;
+  pgVgRatio: string;
+  nicotinRatio: number;
+  aromas: {
+    aromaId?: string | undefined;
+    aromaName: string;
+    aromaBrand: string;
+    aromaProportion: number;
+    aromaQuantity: number;
+  }[];
+
+  constructor(
+    name: string,
+    pgVgRatio: string,
+    nicotinRatio: number,
+    aromas: Recipe["aromas"]
+  ) {
+    this.name = name;
+    this.pgVgRatio = pgVgRatio;
+    this.nicotinRatio = nicotinRatio;
+    this.aromas = aromas;
+  }
+
+  static getFromFormEntries(formDataObject: {
+    [key: string]: FormDataEntryValue;
+  }): Recipe {
+    const aromaSet = new Set<string>();
+    let aromas: Recipe["aromas"] = [];
+
+    const nonAromaFields = {
+      name: formDataObject.recipeName as string,
+      pgVgRatio: formDataObject.pgVgRatio as string,
+      nicotinRatio: Number(formDataObject.nicotinRatio),
+    };
+
+    for (const key in formDataObject) {
+      key.includes("aroma") && key.length < 8 && aromaSet.add(key);
+    }
+    for (const aroma of aromaSet) {
+      aromas = [
+        ...aromas,
+        {
+          aromaName: formDataObject[aroma] as string,
+          aromaBrand: formDataObject[aroma + "_brand"] as string,
+          aromaProportion: Number(formDataObject[aroma + "_proportion"]),
+          aromaQuantity: Number(formDataObject[aroma + "_quantity"]),
+        },
+      ];
+    }
+    return { ...nonAromaFields, aromas };
+  }
+}
